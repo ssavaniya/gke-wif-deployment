82e542fbb73013e77764c0e2251a584725143e44
Sachin Savaniya <sachin.savaniya@gmail.com>
2026-06-25 04:26:52 +0000

    Fix ingress endpoint

--- src/main/java/com/sachin/hello_gke/HelloController.java
+++ src/main/java/com/sachin/hello_gke/HelloController.java
@@ -10,17 +10,17 @@ public class HelloController {
 
     @GetMapping("/")
     public Map<String, String> loadBalancer() {
-
         return Map.of(
                 "message", "Hello from Load Balancer",
+                "environment", "dev"
         );
     }
 
     @GetMapping("/ingress")
     public Map<String, String> ingress() {
-
         return Map.of(
                 "message", "Hello from Ingress",
+                "environment", "dev"
         );
     }
 }
